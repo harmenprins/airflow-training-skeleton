a9f427f0a617f86214dc2577b70622882168b548
hprins <hprins@bol.com>
2018-11-20 12:11:36 +0100

    refactor

--- dags/newdag.py
+++ dags/newdag.py
@@ -17,8 +17,7 @@ p = print_days_till_christmas
 from airflow.operators.bash_operator import BashOperator
 
 
-def SleepOperator(s: int = 5):
-    return BashOperator(task_id="wait_" + str(s), bash_command= "sleep " + str(s))
+
 
 
 with DAG(
@@ -28,9 +27,9 @@ with DAG(
     print_days_till_christmas = PythonOperator(task_id="print_days_till_christmas",
                                                python_callable=print_days_till_christmas,
                                                default_args=args)
-    wait_5 = SleepOperator(5)
-    wait_1 = SleepOperator(1)
-    wait_10 = SleepOperator(10)
+    wait_5 = BashOperator(task_id="wait_1", bash_command="sleep 1")
+    wait_1 = BashOperator(task_id="wait_5", bash_command="sleep 5")
+    wait_10 = BashOperator(task_id="wait_10", bash_command="sleep 10")
     the_end = DummyOperator(task_id="the_end")
 
 print_days_till_christmas >> [wait_1, wait_5, wait_10]
