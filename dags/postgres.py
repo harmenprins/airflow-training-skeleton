83a12c9579cc05534750aa7f0bc9a1846343dad2
hprins <hprins@bol.com>
2018-11-22 11:20:25 +0100

    Added dataproc part of flow.

--- dags/postgres.py
+++ dags/postgres.py
@@ -8,6 +8,7 @@ from pendulum import Pendulum
 
 args = {"owner": "Harmen", "start_date": airflow.utils.dates.days_ago(3)}
 dag = DAG(dag_id="exercise4",default_args=args)
+project_id = "airflowbolcom-6b5317463050ef21"
 
 host = '178.62.227.89'
 username = "gdd"
@@ -22,3 +23,31 @@ pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     postgres_conn_id="training_postgres",
     dag=dag
 )
+
+from airflow.contrib.operators.dataproc_operator import *
+
+dataproc_create_cluster = DataprocClusterCreateOperator(
+    task_id="create_dataproc",
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id=project_id,
+    num_workers=2,
+    zone="europe-west4-a",
+    dag=dag
+)
+
+compute_aggregates = DataProcPySparkOperator(
+    task_id="compute_aggregates",
+    main="gs://europe-west1-training-airfl-596abff0-bucket/build_statistics.py",
+    cluster_name="analyse-pricing-{{ ds }}",
+    arguments=["{{ ds }}"],
+    dag=dag
+)
+
+dataproc_delete_cluster = DataprocClusterDeleteOperator(
+    task_id="delete_dataproc",
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id=project_id,
+    dag=dag
+)
+
+pgsl_to_gcs >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
