d417f1ff24cb585911cbdd34035f68daffd3a10d
hprins <hprins@bol.com>
2018-11-22 14:32:46 +0100

    k

--- dags/postgres.py
+++ dags/postgres.py
@@ -1,10 +1,14 @@
+from tempfile import NamedTemporaryFile
+
 import airflow
 from airflow import DAG
+from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+from airflow.hooks.http_hook import HttpHook
 from airflow.utils.trigger_rule import TriggerRule
 
 args = {"owner": "Harmen", "start_date": airflow.utils.dates.days_ago(3)}
-dag = DAG(dag_id="exercise4",default_args=args)
+dag = DAG(dag_id="exercise4", default_args=args)
 project_id = "airflowbolcom-6b5317463050ef21"
 
 host = '178.62.227.89'
@@ -47,4 +51,70 @@ dataproc_delete_cluster = DataprocClusterDeleteOperator(
     dag=dag
 )
 
+
+class HttpToGcsOperator(BaseOperator):
+    """
+    Calls an endpoint on an HTTP system to execute an action
+
+    :param http_conn_id: The connection to run the operator against
+    :type http_conn_id: string
+    :param endpoint: The relative part of the full url. (templated)
+    :type endpoint: string
+    :param gcs_path: The path of the GCS to store the result
+    :type gcs_path: string
+    """
+
+    template_fields = ("endpoint", "gcs_path")
+    template_ext = ()
+    ui_color = "#f4a460"
+
+    @apply_defaults
+    def __init__(
+            self,
+            endpoint,
+            gcs_bucket,
+            gcs_path,
+            method="GET",
+            http_conn_id="http_default",
+            gcs_conn_id="google_cloud_default",
+            *args,
+            **kwargs
+    ):
+        super(HttpToGcsOperator, self).__init__(*args, **kwargs)
+        self.http_conn_id = http_conn_id
+        self.method = method
+        self.endpoint = endpoint
+        self.gcs_bucket = gcs_bucket
+        self.gcs_path = gcs_path
+        self.gcs_conn_id = gcs_conn_id
+
+    def execute(self, context):
+        http = HttpHook(self.method, http_conn_id=self.http_conn_id)
+
+        self.log.info("Calling HTTP method")
+        response = http.run(self.endpoint)
+
+        with NamedTemporaryFile() as tmp_file_handle:
+            tmp_file_handle.write(response.content)
+            tmp_file_handle.flush()
+
+            hook = GoogleCloudStorageHook(google_cloud_storage_conn_id=self.gcs_conn_id)
+            hook.upload(
+                bucket=self.gcs_bucket,
+                object=self.gcs_path,
+                filename=tmp_file_handle.name,
+            )
+
+
+http2gcs = HttpToGcsOperator(
+    endpoint="https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date=2018-01-01&from=GBP&to=EUR",
+    gcs_bucket="europe-west1-training-airfl-596abff0-bucket",
+    gcs_path="currency/",
+    task_id="delete_dataproc",
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id=project_id,
+    dag=dag
+)
+
 pgsl_to_gcs >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
+http2gcs >> dataproc_create_cluster
